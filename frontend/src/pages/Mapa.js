1cbd67175cf0f83cf1ce5f98390a25ec6e610329
Manuel Acosta <macosta.ia88@gmail.com>
2026-06-17 15:42:20 -0600

    Revert "feat(F4): Mapa de Valores con navbar unificado (PublicNav en LightScope)"
    This reverts commit 8482ce5a4a70f809247fe8f6fa97e1b6d252d5ea.

--- frontend/src/pages/Mapa.js
+++ frontend/src/pages/Mapa.js
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { LightScope, PublicNav } from '../components/ui';
+import Navbar from '../components/landing/Navbar';
 import { fetchColonias } from '../api/marketplace';
 import { X, ArrowRight } from '../components/icons';
 import { Z } from '../styles/zIndex';
@@ -436,8 +436,8 @@ export default function Mapa({ user, onLogin, onLogout }) {
   }, [mapReady]);
 
   return (
-    <LightScope>
-      <PublicNav />
+    <div style={{ background: 'var(--bg)', minHeight: '100vh' }}>
+      <Navbar user={user} onLogin={onLogin} onLogout={onLogout} />
       <main style={{ paddingTop: 60, position: 'relative', height: 'calc(100vh - 60px)' }}>
         {!TOKEN && (
           <div style={{
@@ -690,6 +690,6 @@ export default function Mapa({ user, onLogin, onLogout }) {
           );
         })()}
       </main>
-    </LightScope>
+    </div>
   );
 }
